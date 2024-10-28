5acb1bf4cfd0739c5f4b7e49d19831abc7ddae10
jpatrick5402 <jpatrick5402@gmail.com>
2024-10-28 09:55:33 -0400

    re-factoring to allow options

--- bits.c
+++ bits.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
-    FILE *fptr;
-    if (!argv[1])
-    {
-        printf("Enter file path to read: ");
-        char answer[1000];
-        scanf("%s", answer);
-        fptr = fopen(answer, "rb");
-    }
-    else
-    {
-        fptr = fopen(argv[1], "rb");
-    }
+void print_help() {
+    printf("bits <FILEPATH> [-r,-v,-h]\n");
+}
 
+void print_raw(FILE * file_pointer) {
+	char * bitString;
+    unsigned char byte;
+    while (fread(&byte, sizeof(byte), 1, file_pointer) == 1) {
+        for (int i = 7; i >= 0; i--) {
+			bitString = bitString + (byte >> i);
+            printf("%d", (byte >> i) & 1); // Print bit
+        }
+        printf("\n");
+    }
+    fclose(file_pointer);
+}
+
+void print_verbose(FILE * file_pointer) {
     int byteCount = 0;
     unsigned char byte;
-    while (fread(&byte, sizeof(byte), 1, fptr) == 1)
+    while (fread(&byte, sizeof(byte), 1, file_pointer) == 1)
     {
         printf("Byte #: %-10d ", byteCount); // Index of Byte
 
@@ -46,6 +50,33 @@ int main(int argc, char *argv[])
         byteCount = byteCount + 1;
     }
     printf("\n");
-    fclose(fptr);
+    fclose(file_pointer);
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *fptr;
+    if (!argv[1])
+    {
+        printf("Enter file path to read: ");
+        char answer[1000];
+        scanf("%s", answer);
+        fptr = fopen(answer, "rb");
+    }
+    else
+    {
+        fptr = fopen(argv[1], "rb");
+    }
+
+    if (strcmp(argv[2], "-h") == 0) {
+        print_help();
+    } else if (strcmp(argv[2], "-r") == 0) {
+        print_raw(fptr);
+    } else if (strcmp(argv[2], "-v") == 0) {
+        print_verbose(fptr);
+    } else {
+        printf("argument not recognized: %s\n", argv[2]);
+    }
+
     return 0;
 }
